2a26f5253107b987e8e13d328a27d09137c5298c
Nhu <tran.hoang.nhu.1997@gmail.com>
2022-08-21 19:04:43 +0700

    fix layout 4

--- inputTextarea.js
+++ inputTextarea.js
@@ -1,11 +1,11 @@
-function inputPrismABAP() {
-  let txtArea = document.querySelector("#txtHtmlStr").value;
-  return `
-    <pre class="line-numbers mx-3"><code class="language-ABAP">${txtArea}</code>
-    </pre>
-    `;
-}
+// function inputPrismABAP() {
+//   let txtArea = document.querySelector("#txtHtmlStr").value;
+//   return `
+//     <pre class="line-numbers mx-3"><code class="language-ABAP">${txtArea}</code>
+//     </pre>
+//     `;
+// }
 
-document.getElementById("btn-swap").onclick = function renderPrismABAP() {
-  document.getElementById("outABAP_code").innerHTML = inputPrismABAP();
-};
+// document.getElementById("btn-swap").onclick = function renderPrismABAP() {
+//   document.getElementById("outABAP_code").innerHTML = inputPrismABAP();
+// };
